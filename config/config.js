9abf21592bf8e7d64147533ea7aa57434a234ce7
YM <ym31316120@gmail.com>
2019-03-14 16:46:38 +0800

    添加对umi的代码文件夹配置的说明

--- config/config.js
+++ config/config.js
@@ -1,1 +1,8 @@
-export default {}
+/* *
+在 umi 中，约定的存放页面代码的文件夹是 pages，是复数，
+不过如果你喜欢单数的话你配置项中你可以添加 singular 为 true 来让 page 变为约定的文件夹。
+我这使用默认的src/pages文件夹效果
+ */
+export default {
+    // singular: true, //
+}
