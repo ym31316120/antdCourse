0a8f4337602405f941302605d212ced634fed7d0
YM <ym31316120@gmail.com>
2019-03-14 17:36:49 +0800

    掌握props.children属性
    1、props.children属性非常重要，该属性表示获取使用某组件时，其元素<XX>???</XX>所填充的???内容，来方便在组件内部进行渲染处理

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -4,7 +4,9 @@ import HelloList from './HelloList';
 class Content extends React.Component{
     render(){
         return (
-          <HelloList name="你好"></HelloList>
+          <HelloList name="你好">
+            <h1>{'标题'}</h1>
+            </HelloList>
         );
     }
 }
--- src/pages/HelloList.js
+++ src/pages/HelloList.js
@@ -3,12 +3,15 @@ import React from 'react';
 class HelloList extends React.Component {
     render() {
         return (
-
+            <div>
+            {this.props.children}
             <ul>
                 <li> Instagram {this.props.name} </li>
                 <li> WhatsApp {this.props.name}</li>
                 <li> Oculus {this.props.name}</li>
+
             </ul>
+        </div>
         );
     }
 }
